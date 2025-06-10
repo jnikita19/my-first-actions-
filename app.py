47d2e854064a5093d9c38edbb9e0fbc6465c147a
AmanjotSinghSaini-int <amanjot.saini@opstree.com>
2025-06-10 17:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,5 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    aws_access_key_id = "AKIA1234567890ABCDEF"
-    aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" #fake key added to test gitleaks
     print("The sum is:", add(10, 5))
 
